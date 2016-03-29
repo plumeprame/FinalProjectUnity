61317a37d89a137e6b01fd7b5c7fcec545c89a02
sujin <sujinchaichanamongkol@gmail.com>
2016-03-29 23:22:53 +0700

    StartUnityProject

---
+++ Assets/controller.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class controller : MonoBehaviour {
+//	public GameObject cam = CharacterController;
+	public float speed = 3.0F;
+	Cardboard cb = new Cardboard();
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float g = Input.GetAxis ("Vertical")*speed;
+		if (Input.GetKey(KeyCode.LeftArrow)) {
+			Debug.Log("key pressed");
+			transform.Translate(Vector3.left * Time.deltaTime * speed, Camera.main.transform);
+		}
+		if (Input.GetKey(KeyCode.RightArrow)) {
+			Debug.Log("key pressed");
+			transform.Translate(Vector3.right * Time.deltaTime * speed, Camera.main.transform);
+		}
+		if (Input.GetKey(KeyCode.UpArrow)) {
+			Debug.Log("key pressed");
+			transform.Translate(Vector3.forward * Time.deltaTime * speed, Camera.main.transform);
+		}
+		if (Input.GetKey(KeyCode.DownArrow)) {
+			Debug.Log("key pressed");
+			transform.Translate(Vector3.back * Time.deltaTime * speed, Camera.main.transform);
+		}
+		//var dir = transform.TransformDirection(Vector3.forward);
+		//dir.z *= speed;
+//		cam.Move(dir * Time.deltaTime);
+		/*float translationy = Input.GetAxis ("Vertical") * speed;
+		float translation = Input.GetAxis("Horizontal") * speed;
+		translation *= Time.deltaTime;
+		translationy *= Time.deltaTime;
+		transform.Translate (translation,0,translationy);*/
+	}
+}
---
+++ Assets/talkto.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class talkto : MonoBehaviour {
+
+	public int TextBoxOnCheck = 0; // Checks if TextBox is on the screen
+	public GameObject MessageBox; // Defines the message box as an object
+	public GameObject TextBox; // Defines the text box as an object
+	string TextMessage; // The text we can change
+	//GameObject QuestBox; // Defines the quest box
+	//GameObject QuestText; // Defines the text in the Quest box
+	//String QuestName; // the quest name we can change
+	// Use this for initialization
+
+
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnMouseDown(){
+		if (TextBoxOnCheck == 0) {
+			TextBoxOnCheck = 1; //Sets the variable to turn the box off
+			MessageBox.SetActive(true); //Re-enables the box
+			TextBox.GetComponent<Text>().text = TextMessage; //changes the message
+			
+		}
+
+		else {
+			TextBoxOnCheck = 0;
+			MessageBox.SetActive(false);
+			TextMessage = "Villager: Get going then!";
+		}
+	}
+
+	}
+
